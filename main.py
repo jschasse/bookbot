b3ac5ecc284ec93f483b17498c5250282514b084
jschasse <jaredchasse4@msn.com>
2024-12-31 20:01:34 -0500

    made code more modular and readable

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 def main():    
     
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+    file_contents = get_book_text("books/frankenstein.txt")
     
     print(file_contents)
 
-    words = []
-    words = file_contents.split()
-    print(len(words))
+    num_words = get_num_words(file_contents)
+    print(num_words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
 main()
